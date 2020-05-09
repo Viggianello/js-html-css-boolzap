a5c5913a4925f2c28d42d71a1064b15b1b0b776e
Antonello Viggiano <antonelloviggiano1990@hotmail.it>
2020-05-09 12:43:17 +0200

    inseriti h1 in html

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-// $('.riga').each(function () {
+//metodo 1
+ // $('.riga').each(function () {
 //     $(this).append('<p>Non poteva essere altrimenti</p>')
 // })
-
+// metodo 2
 $('.riga').append('<p>Non poteva essere altrimenti</p>')
+
+// $('.riga').append('<h1>Michele</h1>')
