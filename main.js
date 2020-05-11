8a637f0228c5df395aa4450d5fe0880646a273a1
Antonello Viggiano <antonelloviggiano1990@hotmail.it>
2020-05-11 12:19:41 +0200

    working progress placeholder effect fixed

--- main.js
+++ main.js
@@ -6,3 +6,48 @@
 $('.riga').append('<p>Non poteva essere altrimenti</p>')
 
 $('.riga').append('<span class="orario">17.55</span>')
+// controllo se il pulsante invio e stato cliccato
+// if (PremoPulsanteInvio()) {
+//     var messaggio_utente = $('#input-messaggi').val();
+//     console.log(messaggio_utente);
+// }
+// Intercetto il clicco sul microfono
+$('.icone-container-right').click(function() {
+    // leggo il valore destritto inserito nel input
+    var messaggio_utente = $('#input-messaggi').val();
+    // stampo in console
+    console.log(messaggio_utente);
+});
+
+// inposto un controllo per l'input
+var controllo_testo_default = true;
+
+// Creo una funzione al click dell'input
+$('#input-messaggi').click(function() {
+    // entra se è il un click dispari sul input
+    if (controllo_testo_default == true) {
+        // tiro via il messaggio di default sull input
+        var testo_default = $(this).attr('placeholder', '');
+        // inposto il controllo a false cosi poi posso ristabilire la condizione di partenza a un click pari
+        controllo_testo_default = false;
+    }
+    else {
+        // il click sul punsante è pari dunque ristabilisco il messaggio di default
+        var testo_default = $(this).attr('placeholder', 'Scrivi un messaggio');
+        // inposto il controllo a true cosi poi posso levare ancora il messaggio di default ad un click dispari
+        controllo_testo_default = true;
+    }
+});
+
+
+
+// funzione che se uso il tasto invio mi da true
+function PremoPulsanteInvio(event){
+  var keyCode = event.which;
+  if (keyCode === 13){
+    //istruzioni da eseguire
+    return false;
+  }else{
+    return true;
+  }
+}
