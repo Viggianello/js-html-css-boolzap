707d2c53e1c8ecdefd8ffd9bad192e4a3407c681
Antonello Viggiano <antonelloviggiano1990@hotmail.it>
2020-05-11 13:02:40 +0200

    inserisco i messaggi nella lista-messaggi tramite l input dell utente

--- main.js
+++ main.js
@@ -11,13 +11,24 @@ $('.riga').append('<span class="orario">17.55</span>')
 //     var messaggio_utente = $('#input-messaggi').val();
 //     console.log(messaggio_utente);
 // }
+var ok = false;
 // Intercetto il clicco sul microfono
 $('.icone-container-right').click(function() {
     // leggo il valore destritto inserito nel input
     var messaggio_utente = $('#input-messaggi').val();
     // stampo in console
     console.log(messaggio_utente);
+    var ilMioNuovoMessaggio = $('.template .nuovo-messaggio').clone();
+    ilMioNuovoMessaggio.text(messaggio_utente);
+    $('.lista-messaggi').append(ilMioNuovoMessaggio);
+    // ok = true;
+    // var testo_default = $('#input-messaggi').attr('placeholder', 'Scrivi un messaggio');
 });
+
+// if (ok) {
+    // rinposto il messaggio di testo_default
+//     var testo_default = $('#input-messaggi').attr('placeholder', 'Scrivi un messaggio');
+// }
 
 // inposto un controllo per l'input
 var controllo_testo_default = true;
@@ -32,14 +43,13 @@ $('#input-messaggi').click(function() {
         controllo_testo_default = false;
     }
     else {
+
         // il click sul punsante è pari dunque ristabilisco il messaggio di default
         var testo_default = $(this).attr('placeholder', 'Scrivi un messaggio');
         // inposto il controllo a true cosi poi posso levare ancora il messaggio di default ad un click dispari
         controllo_testo_default = true;
     }
 });
-
-
 
 // funzione che se uso il tasto invio mi da true
 function PremoPulsanteInvio(event){
